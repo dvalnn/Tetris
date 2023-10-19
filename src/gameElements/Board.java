3028f6a5eb0bd623da50b99d7b547145e25a70b3
Tiago Amorim <tiago.andre.amorim@gmail.com>
2023-10-19 22:16:55 +0100

    Removed deprecated constant variables

--- src/gameElements/Board.java
+++ src/gameElements/Board.java
@@ -3,28 +3,27 @@ package gameElements;
 import java.awt.Color;
 import java.awt.Graphics;
 
+import static utils.Constants.GameConstants.*;
+
 // GamePanel is a JPanel -- a container for all visual elements in the game
 public class Board {
     private int squareSize;
-    private int width, height;
-
     private int x0, y0;
 
-    private int[][] grid = new int[height][width];
+    // private int[][] grid = new int[BOARD_WIDTH][BOARD_WIDTH];
     private Color bkgColor = Color.BLACK;
 
     private Tetromino tetromino;
     // private Tetromino nextTetromino;
 
-    public Board(int squareSize, int width, int height, int offsetX, int offsetY, Color bkgColor) {
+    public Board(int squareSize, int offsetX, int offsetY, Color bkgColor) {
         this.squareSize = squareSize;
-        this.width = width;
-        this.height = height;
+
         this.x0 = offsetX;
         this.y0 = offsetY;
         this.bkgColor = bkgColor;
 
-        int tetroX = (offsetX + width) / 2 * squareSize;
+        int tetroX = (offsetX + BOARD_WIDTH) / 2 * squareSize;
 
         // squareSize is the size of each square in the tetromino
         // tetroX is the initial x position of the tetromino
@@ -40,19 +39,19 @@ public class Board {
     public void render(Graphics g) {
         // draws the background
         g.setColor(bkgColor);
-        g.fillRect(x0, y0, width * squareSize, height * squareSize);
+        g.fillRect(x0, y0, BOARD_WIDTH * squareSize, BOARD_HEIGHT * squareSize);
 
         // draws the tetromino
         tetromino.render(g);
 
         // draws the grid
-        for (int row = 0; row < height; row++) {
+        for (int row = 0; row < BOARD_HEIGHT; row++) {
             g.setColor(Color.WHITE);
-            g.drawLine(x0, row * squareSize, width * squareSize, row * squareSize);
+            g.drawLine(x0, row * squareSize, BOARD_WIDTH * squareSize, row * squareSize);
         }
-        for (int col = 0; col < width; col++) {
+        for (int col = 0; col < BOARD_WIDTH; col++) {
             g.setColor(Color.WHITE);
-            g.drawLine(col * squareSize, y0, col * squareSize, height * squareSize);
+            g.drawLine(col * squareSize, y0, col * squareSize, BOARD_HEIGHT * squareSize);
         }
 
     }
