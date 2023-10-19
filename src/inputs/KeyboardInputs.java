a980d7a6c76ffcea95e575f89bb4ab0aface505c
Tiago Amorim <tiago.andre.amorim@gmail.com>
2023-10-20 00:19:18 +0100

    keyboard input processing for the tetrominos

--- src/inputs/KeyboardInputs.java
+++ src/inputs/KeyboardInputs.java
@@ -4,6 +4,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import main.GamePanel;
+import static utils.Constants.Directions.*;
 
 public class KeyboardInputs implements KeyListener {
 
@@ -22,16 +23,20 @@ public class KeyboardInputs implements KeyListener {
     public void keyPressed(KeyEvent e) {
         switch (e.getKeyCode()) {
             case (KeyEvent.VK_W):
+            case (KeyEvent.VK_UP):
                 //
                 break;
             case (KeyEvent.VK_A):
-                //
+            case (KeyEvent.VK_LEFT):
+                gamePanel.getBoard().getTetromino().move(LEFT);
                 break;
             case (KeyEvent.VK_S):
-                //
+            case (KeyEvent.VK_DOWN):
+                gamePanel.getBoard().getTetromino().fastDrop();
                 break;
             case (KeyEvent.VK_D):
-                //
+            case (KeyEvent.VK_RIGHT):
+                gamePanel.getBoard().getTetromino().move(RIGHT);
                 break;
         }
     }
