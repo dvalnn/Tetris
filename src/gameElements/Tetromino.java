b8fa2aff74ccded1d0e4433cb6ee7aee8f75158c
Tiago Amorim <tiago.andre.amorim@gmail.com>
2023-10-19 22:14:57 +0100

    Tetromino update movement logic

--- src/gameElements/Tetromino.java
+++ src/gameElements/Tetromino.java
@@ -2,29 +2,40 @@ package gameElements;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.util.Random;
+
+import static utils.Constants.GameConstants.*;
+import static utils.Constants.TetrominoConstants.*;
 
 public class Tetromino {
     private int x;
     private int y;
     private int size;
 
+    // 2D array representing the shape of the tetromino
     private int[][] shape = {
+            { 0, 0, 0 },
             { 0, 1, 0 },
-            { 1, 1, 1 },
-            { 0, 0, 0 }
+            { 1, 1, 1 }
     };
 
     private Color color;
 
-    private Random rand;
+    // * move tick, move delay and move speed are used to control the speed of
+    // * the tetromino move tick will be incremented every time the update method
+    // * is called, this means that the moveTick will count at the same rate as the
+    // * UPS. move delay is the number of ticks that must pass before the tetromino
+    // * moves down one square. This is set to the UPS so that the tetromino moves
+    // * down one square every second, by default. By varying the move speed we
+    // * can change the number of ticks that must pass before the tetromino moves.
+    private int moveTick = 0;
+    private int moveSpeed = 1;
+    private final int moveDelay = UPS_SET;
 
     public Tetromino(int x, int y, int size) {
         this.x = x;
         this.y = y;
         this.size = size;
-        this.rand = new Random();
-        this.color = getRandColor();
+        this.color = new Color(T_COLOR[0][0], T_COLOR[0][1], T_COLOR[0][2]);
     }
 
     public void moveX(int x) {
@@ -44,7 +55,13 @@ public class Tetromino {
     }
 
     public void update() {
-        // this.y += size;
+        moveTick++;
+        if (moveTick >= moveDelay * moveSpeed) {
+            moveTick = 0;
+            if (y + 3 * size < BOARD_HEIGHT * size) {
+                moveY(size);
+            }
+        }
     }
 
     public void render(Graphics g) {
@@ -58,11 +75,4 @@ public class Tetromino {
         }
     }
 
-    private Color getRandColor() {
-        int r = rand.nextInt(255);
-        int g = rand.nextInt(255);
-        int b = rand.nextInt(255);
-
-        return new Color(r, g, b);
-    }
 }
