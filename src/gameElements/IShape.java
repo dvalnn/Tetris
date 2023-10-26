489dfe410a48dfa7846b2b2e593110b9d0942fd5
Tiago Amorim <tiago.andre.amorim@gmail.com>
2023-10-26 14:28:14 +0100

    overriding super class move and rotate methods

--- src/gameElements/IShape.java
+++ src/gameElements/IShape.java
@@ -15,8 +15,66 @@ public class IShape extends Shape {
   private static final Point2D ORIGIN = new Point2D.Double(1.5, 0.5);
   private static final Color COLOR = new Color(0, 255, 255);
 
+  private int minX, maxX, minY, maxY;
+  private boolean vertical = false;
+  private boolean horizontal = true;
+
   public IShape(int renderSize, Point2D renderOffset) {
     super(ORIGIN, SHAPE, COLOR, renderSize, renderOffset);
+    calculateMinMaxCoords();
+  }
+
+  private void calculateMinMaxCoords() {
+    if (horizontal) {
+      minY = maxY = (int) body[0].getY();
+      minX = (int) Math.min(body[0].getX(), body[3].getX());
+      maxX = (int) Math.max(body[0].getX(), body[3].getX());
+      return;
+    }
+
+    minX = maxX = (int) body[0].getX();
+    minY = (int) Math.min(body[0].getY(), body[3].getY());
+    maxY = (int) Math.max(body[0].getY(), body[3].getY());
+
+  }
+
+  public void move(int x, int y) {
+    super.move(x, y);
+    minX += x;
+    maxX += x;
+    minY += y;
+    maxY += y;
+  }
+
+  public void rotate(double angle) {
+    System.out.println("----------------------");
+    System.out.println("Before rotation");
+    System.out.println("isVertical: " + vertical);
+    System.out.println("isHorizontal: " + horizontal);
+    System.out.println("minX " + minX + " maxX " + maxX);
+    System.out.println("minY " + minY + " maxY " + maxY);
+    System.out.println("Points:");
+
+    for (Point2D point : body) {
+      System.out.println(point);
+    }
+
+    super.rotate(angle);
+
+    vertical = !vertical;
+    horizontal = !horizontal;
+
+    calculateMinMaxCoords();
+    System.out.println("After rotation");
+    System.out.println("isVertical: " + vertical);
+    System.out.println("isHorizontal: " + horizontal);
+    System.out.println("minX " + minX + " maxX " + maxX);
+    System.out.println("minY " + minY + " maxY " + maxY);
+    System.out.println("Points:");
+
+    for (Point2D point : body) {
+      System.out.println(point);
+    }
   }
 
 }
