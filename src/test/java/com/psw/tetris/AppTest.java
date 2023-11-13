2aede269ef4fb4a3be28692fa672eb3c4385f7a4
lucasgmf <lucasgalvaomfreitas@gmail.com>
2023-11-13 19:10:38 +0000

    unit tests for shape factory using junit5

--- src/test/java/com/psw/tetris/AppTest.java
+++ src/test/java/com/psw/tetris/AppTest.java
@@ -1,8 +1,8 @@
 package com.psw.tetris;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /** Unit test for simple App. */
 public class AppTest {
