84ad4c10869267b6243ea66a5ef0297dcbe2f358
Tiago Amorim <tiago.andre.amorim@gmail.com>
2023-11-16 12:33:29 +0000

    fixed imports and typo

--- src/test/java/com/psw/tetris/Networking/Packet/PacketTest.java
+++ src/test/java/com/psw/tetris/Networking/Packet/PacketTest.java
@@ -1,13 +1,19 @@
+package com.psw.tetris.Networking.Packet;
+
 import com.psw.tetris.networking.GameClient;
 import com.psw.tetris.networking.GameServer;
-import org.junit.jupiter.api.Test;
 
-public class Packet00LoginTest{
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.psw.tetris.networking.packets.Packet00Login;
+
+public class PacketTest {
   @Test
-  public void testConstructor1(){
-  String username = "tiago";
-  String data  = "00," + username;
+  public void testConstructor1() {
+    String username = "tiago";
+    String data = "00," + username;
     Packet00Login newP = new Packet00Login(data.getBytes());
-    AssertNotNull(data);  
+    assertNotNull(data);
   }
 }
