0438b123c62bfa653df3f69472ac8e5cf5bceb0a
Tiago Amorim <tiago.andre.amorim@gmail.com>
2023-12-03 11:27:21 +0000

    fixed some imports and minor code cleanup

--- src/main/java/com/psw/tetris/utils/LeaderBoard.java
+++ src/main/java/com/psw/tetris/utils/LeaderBoard.java
@@ -1,10 +1,14 @@
 package com.psw.tetris.utils;
+
 import java.io.*;
 import java.io.Serializable;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Formatter;
+
+import com.psw.tetris.gameStates.GameStateHandler;
+import com.psw.tetris.gameStates.GameStateHandler.GameStatesEnum;
 
 import static com.psw.tetris.gameElements.gameplay.GameTime.getTimeStr;
 import static com.psw.tetris.gameElements.gameplay.Levels.getCurrentLevel;
@@ -15,55 +19,57 @@ import static com.psw.tetris.utils.Constants.RESOURCES_PATH;
 import static com.psw.tetris.utils.Constants.UI.Buttons.LEADERBOARD;
 
 public class LeaderBoard implements Serializable {
-    private int TotalScore;
+  private static int index;
 
-    private String TimePlayed;
+  private int TotalScore;
+  private String TimePlayed;
+  private int LastLevel;
 
-    private int LastLevel;
+  private static LeaderBoard[] TopScore = new LeaderBoard[10];
 
-    public static void update() throws IOException {
-
-        LeaderBoard[] TopScore = new LeaderBoard[10];
-
-        for(int i = 0; i < 10; i++) {
-
-            TopScore[i] = new LeaderBoard();
-        }
-        int j = 0;
-        if (getActiveState().equals(GAME_OVER)) {
-
-            TopScore[j].TotalScore = getScore();
-            TopScore[j].LastLevel = getCurrentLevel();//in progress
-            TopScore[j].TimePlayed = getTimeStr();//in progress
-            j = j + 1;
-
-            try {
-                FileOutputStream f1 = new FileOutputStream(RESOURCES_PATH+ "/Scores.txt");
-                ObjectOutputStream out = new ObjectOutputStream(f1);
-                for(int i = 0; i<10 ; i++) {
-                    out.writeObject(TopScore[i]);
-                }
-                out.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            if (getActiveState().equals(LEADERBOARD)){
-
-                try {
-                    FileInputStream f2 = new FileInputStream(RESOURCES_PATH+ "/Scores.txt");
-                    ObjectInputStream in = new ObjectInputStream(f2);
-                    LeaderBoard topScores = (LeaderBoard) in.readObject();
-                    in.close();
-                } catch (ClassNotFoundException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-
-
-        }
+  static {
+    for (int i = 0; i < 10; i++) {
+      TopScore[i] = new LeaderBoard();
     }
+
+    index = 0;
+  }
+
+  public static void update() throws IOException {
+
+    if (GameStateHandler.getActiveStateID().equals(GameStatesEnum.GAME_OVER)) {
+
+      TopScore[index].TotalScore = getScore();
+      TopScore[index].LastLevel = getCurrentLevel();// in progress
+      TopScore[index].TimePlayed = getTimeStr();// in progress
+
+      try {
+        FileOutputStream f1 = new FileOutputStream(RESOURCES_PATH + "/Scores.txt");
+        ObjectOutputStream out = new ObjectOutputStream(f1);
+        for (int i = 0; i < 10; i++) {
+          out.writeObject(TopScore[i]);
+        }
+        out.close();
+        index++;
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+
+      if (GameStateHandler.getActiveStateID().equals(GameStatesEnum.LEADERBOARD)) {
+
+        try {
+          FileInputStream f2 = new FileInputStream(RESOURCES_PATH + "/Scores.txt");
+          ObjectInputStream in = new ObjectInputStream(f2);
+          LeaderBoard topScores = (LeaderBoard) in.readObject();
+          in.close();
+          System.out.println(topScores.TotalScore);
+          System.out.println(topScores.LastLevel);
+          System.out.println(topScores.TimePlayed);
+        } catch (ClassNotFoundException e) {
+          throw new RuntimeException(e);
+        }
+      }
+
+    }
+  }
 }
-
-
-
