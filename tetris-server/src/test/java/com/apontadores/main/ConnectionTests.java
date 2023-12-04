338dd662c1052f5580e5fc99092af13afbe8c9b1
Tiago Amorim <tiago.andre.amorim@gmail.com>
2023-12-04 17:01:38 +0000

    rewrote test

--- tetris-server/src/test/java/com/apontadores/main/ConnectionTests.java
+++ tetris-server/src/test/java/com/apontadores/main/ConnectionTests.java
@@ -3,6 +3,7 @@ package com.apontadores.main;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
@@ -14,64 +15,83 @@ import com.apontadores.packets.Packet00Redirect;
 import com.apontadores.packets.Packet01Login;
 
 public class ConnectionTests {
-  private final static int defaultPort = 42069;
+  private final static int PORT = 42069;
+  private final static String ADDRESS = "127.0.0.1";
+  private final static Server server = new Server(ConnectionTests.PORT);
 
-  @Test
-  public void testServerCreation() {
-    Server s;
-    try {
-      s = new Server(ConnectionTests.defaultPort);
-    } catch (Exception e) {
-      e.printStackTrace();
-      Assertions.fail("Server should not throw an exception");
-      s = null;
-    }
-
-    if (s != null) {
-      s.close();
-    }
-
+  static {
+    new Thread(server).start();
   }
 
-  @Test
-  public void testConnection() throws Exception {
-    Server s = new Server(ConnectionTests.defaultPort);
-
-    assertNotNull(s);
-    new Thread(s).start();
+  public void testPlayerConnection(
+      String playerName,
+      String roomName)
+      throws IOException {
 
     DatagramSocket mockClient = new DatagramSocket();
     mockClient.setSoTimeout(10000);
 
-    System.out.println("[MockClient] client on port: " + mockClient.getLocalPort());
+    System.out.println("[TEST] mock client on port: " + mockClient.getLocalPort());
 
-    Packet01Login loginPacket = new Packet01Login("test", "test");
-    String serverAddress = "127.0.0.1";
-    InetAddress serverInetAddress = InetAddress.getByName(serverAddress);
+    Packet01Login loginPacket = new Packet01Login(playerName, roomName);
+    InetAddress serverInetAddress = InetAddress.getByName(ConnectionTests.ADDRESS);
+
     DatagramPacket outPacket = new DatagramPacket(
         loginPacket.getBytes(),
         loginPacket.getBytes().length,
         serverInetAddress,
-        ConnectionTests.defaultPort);
+        ConnectionTests.PORT);
 
     mockClient.send(outPacket);
 
-    DatagramPacket inPacket = new DatagramPacket(new byte[1024], 1024);
-    mockClient.receive(inPacket);
-    assertNotEquals(0, inPacket.getLength(), "Received packet should not be empty");
+    DatagramPacket inPacket = new DatagramPacket(
+        new byte[Server.MAX_PACKET_SIZE],
+        Server.MAX_PACKET_SIZE);
 
-    Packet00Redirect redirectPacket = Packet00Redirect.fromBytes(inPacket.getData(), inPacket.getLength());
+    mockClient.receive(inPacket);
+    assertNotEquals(0, inPacket.getLength(), "[TEST] Received packet should not be empty");
+
+    Packet00Redirect redirectPacket = Packet00Redirect.fromBytes(
+        inPacket.getData(),
+        inPacket.getLength());
+
     assertNotNull(redirectPacket);
-    System.out.println("[ConnectionTests] Received redirect port: " + redirectPacket.getPort());
+    System.out.println(
+        "[TEST] Received redirect port: "
+            + redirectPacket.getPort());
 
     DatagramPacket outPacket2 = new DatagramPacket(
         loginPacket.getBytes(),
         loginPacket.getBytes().length,
         serverInetAddress,
         redirectPacket.getPort());
-    assertNotNull(outPacket2);
 
+    assertNotNull(outPacket2);
     mockClient.send(outPacket2);
     mockClient.close();
   }
+
+  @Test
+  public void testServer() {
+    assertNotNull(server);
+
+    String p1Name = "testP1";
+    String p2Name = "testP2";
+    String roomName = "testRoom";
+
+    try {
+      testPlayerConnection(p1Name, roomName);
+    } catch (IOException e) {
+      Assertions.fail(e.getMessage());
+    }
+
+    try {
+      testPlayerConnection(p2Name, roomName);
+    } catch (IOException e) {
+      Assertions.fail(e.getMessage());
+    }
+
+    server.close();
+  }
+
 }
